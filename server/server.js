7e94b104254b7966c311aeeedda8fea8cae36a50
Trevor Bos <trevor.bos@outlook.com>
2022-09-20 09:46:47 -0400

    still trying to fix issues.

--- server/server.js
+++ server/server.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 // import ApolloServer
-const { ApolloServer } = require('apollo-server-express');
+const { ApolloServer } = require("apollo-server-express");
 
 // import our typeDefs and resolvers
-const { typeDefs, resolvers } = require('./schemas');
+const { typeDefs, resolvers } = require("./schemas");
 
-const db = require('./config/connection');
+const db = require("./config/connection");
 
-const {authMiddleware} = require('./utils/auth');
+const { authMiddleware } = require("./utils/auth");
 // const routes = require('./routes');
 
 const app = express();
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context:authMiddleware
+  context: authMiddleware,
 });
 
 // integrate our Apollo server with the Express application as middleware
@@ -29,15 +29,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Serve up static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
-db.once('open', () => {
+db.once("open", () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
     // log where we can go to test our GQL API
