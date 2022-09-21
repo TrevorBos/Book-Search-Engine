e39fd1d750c397a90e23805eec473e55d390b1ee
Trevor Bos <trevor.bos@outlook.com>
2022-09-21 15:16:44 -0400

    got the saving functionality fixed with the help of askBCS

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -25,24 +25,11 @@ export const ADD_USER = gql`
 `;
 
 export const SAVE_BOOK = gql`
-  mutation saveBook(
-    $authors: [String]
-    $description: String
-    $title: String
-    $bookId: String
-    $image: String
-    $link: String
-  ) {
-    saveBook(
-      authors: $authors
-      description: $description
-      title: $title
-      bookId: $bookId
-      image: $image
-      link: $link
-    ) {
+  mutation saveBook($bookData: SavedBookInput!) {
+    saveBook(bookData: $bookData) {
       _id
       username
+      email
       savedBooks {
         authors
         description
