9aeb4fde37223366e00aa37d80c188196a18f44f
Trevor Bos <trevor.bos@outlook.com>
2022-09-21 15:42:35 -0400

    Now were having issues viewing the saved books, will look into this more later.

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -6,39 +6,46 @@ import {
   Card,
   Button,
 } from "react-bootstrap";
-import Auth from "../utils/auth";
-import { removeBookId } from "../utils/localStorage";
 import { useQuery, useMutation } from "@apollo/client";
+
+import Auth from "../utils/auth";
+import { removeBookId, saveBookIds } from "../utils/localStorage";
 import { GET_ME } from "../utils/queries";
 import { REMOVE_BOOK } from "../utils/mutations";
 
 const SavedBooks = () => {
   const { loading, data } = useQuery(GET_ME);
-  const [removeBook, { error }] = useMutation(REMOVE_BOOK);
-  const userData = data?.me || {};
+  const userData = data?.me || [];
 
-  // create function that accepts the book's mongo _id value as param and deletes the book from the database
+  const [removeBook, { error }] = useMutation(REMOVE_BOOK);
+
   const handleDeleteBook = async (bookId) => {
     const token = Auth.loggedIn() ? Auth.getToken() : null;
-
     if (!token) {
       return false;
     }
-
     try {
-      const { data } = await removeBook({
-        variables: { bookId },
+      const response = await removeBook({
+        variables: { bookId: bookId },
       });
 
+      if (!response) {
+        throw new Error("something went wrong!");
+      }
       removeBookId(bookId);
     } catch (err) {
-      console.error(err);
+      console.error(error);
     }
   };
 
+  // if data isn't here yet, say so
   if (loading) {
     return <h2>LOADING...</h2>;
   }
+
+  // sync localStorage with what was returned from the userData query
+  const savedBookIds = userData.savedBooks.map((book) => book.bookId);
+  saveBookIds(savedBookIds);
 
   return (
     <>
